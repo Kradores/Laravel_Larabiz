8cda06d05303ea73c4128183c4a97d97fc589529
Alexandru Noroc <kradores10@gmail.com>
2019-07-29 22:22:02 +0300

    CREATED: View, Create, Validate Listing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+                    Dashboard
+                    <a href="/listing/create" class="btn btn-success btn-sm float-right">Add Listing</a></div>
                 <div class="card-body">
                     <h3>Your Listings</h3>
                     @if (count($listings) > 0)
                         <table class="table table-striped">
-                            <tr>
-                                <th>Company</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
+                            <thead>
+                                <tr>
+                                    <th>Company</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
                             @foreach ($listings as $listing)
+                            <tbody>
                                 <tr>
                                     <td>{{$listing->name}}</td>
                                     <td></td>
                                     <td></td>
                                 </tr>
+                            </tbody>
                             @endforeach
                         </table>
                     @endif
@@ -29,5 +33,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
